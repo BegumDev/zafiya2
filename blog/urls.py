fa622a68434887674f5858265efb08849ead37e4
BegumDev <ruk90@outlook.com>
2022-10-30 12:48:13 +0000

    Add CRUD functionality to admin users to add and edit posts.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.view_blog, name='view_blog'),
+    path('read_post/<int:id>', views.read_post, name='read_post'),
+    path('create_post/', views.create_post, name='create_post'),
+    path('update_post/<int:id>', views.update_post, name='update_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.models import User
+from django.contrib import messages
 from .models import BlogPost
 from .forms import BlogForm
 
@@ -12,4 +14,70 @@ def view_blog(request):
     context = {
         'posts': posts,
     }
+    return render(request, template, context)
+
+def read_post(request, id):
+    """ View individual post and be able to comment on them"""
+
+    post = BlogPost.objects.get(id=int(id))
+
+    template = 'blog/read_post.html'
+    context = {
+        'post': post,
+    }
+    return render(request, template, context)
+
+
+def create_post(request):
+    """ View to create a blog post """
+
+    form = BlogForm()
+
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        if form.is_valid:
+            data = form.save(commit=False)
+            data.author = User(id=request.user.id)
+            data.save()
+
+            messages.success(request, 'Blog post successfully created')
+            return redirect(reverse('view_blog'))
+        else:
+            messages.error(request, 'There was an error with the form. Please try again.')
+    else:
+        form = BlogForm()
+    
+    template = 'blog/create_post.html'
+    context = {
+          'form': form,
+      }
+
+    return render(request, template, context)
+
+
+def update_post(request, id):
+    """ View to create a blog post """
+
+    post = BlogPost.objects.get(id=int(id))
+
+    current_info = {
+        'blog_title': post.blog_title,
+        'content': post.content,
+    }
+    form = BlogForm(initial=current_info)
+
+    if request.method == 'POST':
+        form = BlogForm(request.POST, instance=post)
+        if form.is_valid:
+            form.save()
+            messages.success(request, 'Blog post successfully updated')
+            return redirect(reverse('view_blog'))
+        else:
+            messages.error(request, 'There was an error with the form. Please try again.')
+    
+    template = 'blog/update_post.html'
+    context = {
+          'form': form,
+      }
+
     return render(request, template, context)
