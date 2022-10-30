4aac52b4f6a0e5f8c376ddd46374c2d3197c26e3
BegumDev <ruk90@outlook.com>
2022-10-30 23:18:01 +0000

    Add searching ability throughout products.

--- products/views.py
+++ products/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from django.db.models import Q
 
 
 def all_products(request):
     """ A view to show all products """
 
     products = Product.objects.all()
+    query = None
+
+    # seraching for products
+    if request.GET:
+        if 'search' in request.GET:
+            query = request.GET['search']
+            if not query:
+                messages.error(request, "You didn't enter any serach requests")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
-
+    
     return render(request, 'products/products.html', context)
 
 
