07816245ffbff025b9f162e7118bd208832dd166
BegumDev <ruk90@outlook.com>
2022-10-31 16:26:00 +0000

    testing stripe keys in deployment.

--- zafiya2/settings.py
+++ zafiya2/settings.py
@@ -213,10 +213,17 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
+# dev variables
+# STRIPE_CURRENCY = 'gbp'
+# STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
+# STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
+# STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
 
+# deploy variables
 STRIPE_CURRENCY = 'gbp'
-STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
-STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
-STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
