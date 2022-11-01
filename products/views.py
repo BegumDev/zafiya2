c581735b53bf32f5799ee1b48797387b9d42478c
BegumDev <ruk90@outlook.com>
2022-11-01 19:36:00 +0000

    Add functionality to order products into categories.

--- products/views.py
+++ products/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Category
 from django.db.models import Q
 
 
 def all_products(request):
     """ A view to show all products """
-
     products = Product.objects.all()
     query = None
+    category = None
 
-    # seraching for products
     if request.GET:
+        # filter by categories
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        # seraching for products
         if 'search' in request.GET:
             query = request.GET['search']
             if not query:
@@ -23,6 +29,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
     
     return render(request, 'products/products.html', context)
