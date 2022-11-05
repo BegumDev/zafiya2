8a85f083c2e05165867db3bf09acf012ebfeb4ad
BegumDev <ruk90@outlook.com>
2022-11-05 00:16:33 +0000

    Add profile app, views and templates to allow users to store delivery informaiton and order history.

---
+++ profiles/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, get_object_or_404
+from profiles.models import UserProfile
+from .forms import UserProfileForm
+from django.contrib import messages
+from checkout.models import Order
+
+# Create your views here.
+def profile(request):
+    """ A view to display user profiles """
+
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated profile')
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
+    template = 'profiles/profile.html'
+    context = {
+        'form': form,
+        'orders': orders,
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    """ A view to historic orders """
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
+    return render(request, template, context)
